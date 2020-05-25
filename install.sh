04d22a193cc275175fc2e8f0de9affbd9cf1900d
Sumiza <64884509+Sumiza@users.noreply.github.com>
2020-05-25 16:37:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,6 +41,8 @@ sed -i '/pm.min_spare_servers = /c\;pm.min_spare_servers = 1' /etc/php-fpm.d/www
 sed -i '/pm.max_spare_servers = /c\;pm.max_spare_servers = 3' /etc/php-fpm.d/www.conf
 sed -i '/pm.process_idle_timeout = /c\pm.process_idle_timeout = 10s' /etc/php-fpm.d/www.conf
 sed -i '/pm.max_requests = /c\pm.max_requests = 0' /etc/php-fpm.d/www.conf
+rm /etc/my.cnf
+wget --no-check-certificate https://raw.githubusercontent.com/Sumiza/Small-VPS-LEMP-Install/master/my.conf -O /etc/my.cnf
 echo "What is the fully qualified domain name (MyTestDomain.com) dont put the www."
 read -r DOMAINNAMEFQDN
 mkdir /usr/share/nginx/html/"$DOMAINNAMEFQDN"
