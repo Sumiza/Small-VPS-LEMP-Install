1caafd3c68b618f4d6c543eb757f1517683e9c8a
Sumiza <64884509+Sumiza@users.noreply.github.com>
2020-05-25 21:20:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ systemctl enable php-fpm
 systemctl enable mariadb
 systemctl enable nginx
 rm /etc/nginx/nginx.conf
-wget --no-check-certificate https://raw.githubusercontent.com/Sumiza/Small-VPS-LEMP-Install/master/nginx.conf -O /etc/nginx/nginx.conf
+wget https://raw.githubusercontent.com/Sumiza/Small-VPS-LEMP-Install/master/nginx.conf -O /etc/nginx/nginx.conf
 sed -i '/listen = 127.0.0.1:9000/c\listen = /var/run/php-fpm/php-fpm.sock' /etc/php-fpm.d/www.conf
 sed -i '/listen.owner = /c\listen.owner = nginx' /etc/php-fpm.d/www.conf
 sed -i '/listen.group = /c\listen.group = nginx' /etc/php-fpm.d/www.conf
@@ -39,14 +39,14 @@ sed -i '/pm.max_children = /c\pm.max_children = 1' /etc/php-fpm.d/www.conf
 sed -i '/pm.process_idle_timeout = /c\pm.process_idle_timeout = 10s' /etc/php-fpm.d/www.conf
 sed -i '/pm.max_requests = /c\pm.max_requests = 0' /etc/php-fpm.d/www.conf
 rm /etc/my.cnf
-wget --no-check-certificate https://raw.githubusercontent.com/Sumiza/Small-VPS-LEMP-Install/master/my.conf -O /etc/my.cnf
+wget https://raw.githubusercontent.com/Sumiza/Small-VPS-LEMP-Install/master/my.conf -O /etc/my.cnf
 echo "What is the fully qualified domain name (MyTestDomain.com) dont put the www."
 read -r DOMAINNAMEFQDN
 mkdir /usr/share/nginx/html/"$DOMAINNAMEFQDN"
 chmod 755 /usr/share/nginx/html/"$DOMAINNAMEFQDN"
 chown -R nginx:nginx /usr/share/nginx/html/"$DOMAINNAMEFQDN"
 cp /usr/share/nginx/index.html /usr/share/nginx/"$DOMAINNAMEFQDN"/index.html
-wget --no-check-certificate https://raw.githubusercontent.com/Sumiza/Small-VPS-LEMP-Install/master/BlankNginx.conf -O /etc/nginx/conf.d/"$DOMAINNAMEFQDN".conf
+wget https://raw.githubusercontent.com/Sumiza/Small-VPS-LEMP-Install/master/BlankNginx.conf -O /etc/nginx/conf.d/"$DOMAINNAMEFQDN".conf
 sed -i "s/WEBSITENAME/$DOMAINNAMEFQDN/g" /etc/nginx/conf.d/"$DOMAINNAMEFQDN".conf
 systemctl restart nginx && systemctl restart mariadb
 echo "Want to set up letsencrypt now? (y/n) only put y if you have your dns set up already or it will fail" 
