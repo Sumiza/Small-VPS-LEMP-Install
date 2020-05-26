d4540fd663029a90a5681e0db8bcf756c4fe0bf8
Sumiza <64884509+Sumiza@users.noreply.github.com>
2020-05-26 00:46:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+clear
+echo "Please select which install:
+1. Initial install
+2. Adding another website"
+read -r RSP
+if [ "$RSP" = "1" ]; then
 yum update -y
 yum install -y epel-release
 yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
@@ -40,6 +46,10 @@ sed -i '/pm.process_idle_timeout = /c\pm.process_idle_timeout = 10s' /etc/php-fp
 sed -i '/pm.max_requests = /c\pm.max_requests = 0' /etc/php-fpm.d/www.conf
 rm /etc/my.cnf
 wget https://raw.githubusercontent.com/Sumiza/Small-VPS-LEMP-Install/master/my.conf -O /etc/my.cnf
+systemctl restart mariadb
+/usr/bin/mysql_secure_installation
+fi
+#----- Initial install done -----------
 echo "What is the fully qualified domain name (MyTestDomain.com) dont put the www."
 read -r DOMAINNAMEFQDN
 mkdir /usr/share/nginx/html/"$DOMAINNAMEFQDN"
@@ -48,14 +58,13 @@ chown -R nginx:nginx /usr/share/nginx/html/"$DOMAINNAMEFQDN"
 cp /usr/share/nginx/index.html /usr/share/nginx/"$DOMAINNAMEFQDN"/index.html
 wget https://raw.githubusercontent.com/Sumiza/Small-VPS-LEMP-Install/master/BlankNginx.conf -O /etc/nginx/conf.d/"$DOMAINNAMEFQDN".conf
 sed -i "s/WEBSITENAME/$DOMAINNAMEFQDN/g" /etc/nginx/conf.d/"$DOMAINNAMEFQDN".conf
-systemctl restart nginx && systemctl restart mariadb
+systemctl restart nginx
 echo "Want to set up letsencrypt now? (y/n) only put y if you have your dns set up already or it will fail" 
 read -r RSP
 if [ "$RSP" = "y" ]; then
 	certbot --nginx --register-unsafely-without-email
 	(crontab -l ; echo "0 3 */10 * * /usr/bin/certbot renew >/dev/null 2>&1") | crontab 
 fi
-/usr/bin/mysql_secure_installation
 echo "Want to set up a MYSQL Database now? (y/n)" 
 read -r RSP
 if [ "$RSP" = "y" ]; then
@@ -72,12 +81,10 @@ mysql -uroot -p"$rootpasswd" -e "create database $DBNAME;"
 mysql -uroot -p"$rootpasswd" -e "grant all on $DBNAME.* to '$DBUSER' identified by '$DBPASS';"
 fi
 rm install.sh
+systemctl restart mariadb && systemctl restart nginx && systemctl restart php-fpm
 echo "DONE.... make sure to note down this information (if you set up a database):
 ----------------------------------
 MYSQL Database : $DBNAME
 MYSQL User : $DBUSER
 MYSQL Password: $DBPASS
------------------------------------
-Hit enter to reboot"
-read -r RSP
-reboot
+-----------------------------------"
